937dab50b221029e53a8cfb59cc261c8f6a76822
joranvar <bart.post@gmail.com>
2016-01-02 19:08:44 +0100

    Exclude zero-distance entries for point-to-self

--- src/query.sql
+++ src/query.sql
@@ -4,5 +4,5 @@ SELECT [StartPointId] = Points1.PointId
      , [EndPointId] = Points2.PointId
      , [Distance] = Points1.Point.STDistance(Points2.Point)
   FROM Points1 AS Points1
- INNER JOIN Points1 AS Points2 ON Points1.PointId <= Points2.PointId
+ INNER JOIN Points1 AS Points2 ON Points1.PointId < Points2.PointId
  WHERE Points1.Point.STDistance(Points2.Point) < @ThresholdInMeters;
